a0ad5e56d930bdee088f0cdb0f96cbcc4337c2d1
lrdgz <developer0606@gmail.com>
2022-09-06 10:48:32 -0400

    Added home and initial styled components

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,7 +6,7 @@ export interface NavbarInterface {}
 
 const Navbar: React.FC<NavbarInterface> = () => {
 	return(
-		<AppBar position="static">
+		<AppBar position="fixed">
 			<Toolbar>
 				<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
 					React TEST
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import { DataGrid, GridRenderCellParams }  from '@mui/x-data-grid';
+import { People } from '@/data';
 
-
-export interface HomeInterface {}
+export interface HomeInterface {};
 
 const Home: React.FC<HomeInterface> = () => {
-	return <div >Home</div>;
+
+	const pageSize = 5;
+
+	const columns = [
+		// { field: name, headerName: 'Name', flex: 1, minWidth: 150, renderCell: (params: GridRenderCellParams) => <>{params.value + params.row.category}</> },
+		{ field: 'name', headerName: 'Name', flex: 1, minWidth: 150, renderCell: (params: GridRenderCellParams) => <>{params.value}</> },
+		{ field: 'category', headerName: 'Category', flex: 1, minWidth: 150, renderCell: (params: GridRenderCellParams) => <>{params.value}</> },
+		{ field: 'company', headerName: 'Company', flex: 1, minWidth: 150, renderCell: (params: GridRenderCellParams) => <>{params.value}</> },
+	];
+
+	return(
+		<DataGrid
+			columns={columns}
+			rows={People}
+			disableColumnSelector 
+			disableSelectionOnClick 
+			autoHeight
+			pageSize={pageSize}
+			rowsPerPageOptions={[pageSize]}
+			getRowId={(row: any) => row.id}
+		/>
+	)
 };
 
 export default Home;
