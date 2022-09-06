a72083534c1c486568d01c3ae750447a538b57fe
lrdgz <developer0606@gmail.com>
2022-09-06 11:04:26 -0400

    Added favorite people, filter favorite people

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { DataGrid, GridRenderCellParams }  from '@mui/x-data-grid';
 import { People } from '@/data';
+import { Person } from '@/models';
+import { Checkbox } from '@mui/material';
 
 export interface HomeInterface {};
 
 const Home: React.FC<HomeInterface> = () => {
 
 	const pageSize = 5;
+	const [selectedPeople, setSelectedPeople] = useState<Person[]>([]); 
+
+	const findPerson = (person: Person) => !!selectedPeople.find(p => p.id === person.id);
+	const filterPerson = (person: Person) => selectedPeople.filter(p => p.id !== person.id);
+
+	const handleChange = (person: Person) => {
+		setSelectedPeople(findPerson(person) ? filterPerson(person) : [...selectedPeople, person]);
+	}
 
 	const columns = [
 		// { field: name, headerName: 'Name', flex: 1, minWidth: 150, renderCell: (params: GridRenderCellParams) => <>{params.value + params.row.category}</> },
+		{ field: 'actions', headerName: '', width: 50, renderCell: (params: GridRenderCellParams) => <>{
+			<Checkbox size="small" checked={findPerson(params.row)} onChange={ () => handleChange(params.row) } />
+		}</> },
 		{ field: 'name', headerName: 'Name', flex: 1, minWidth: 150, renderCell: (params: GridRenderCellParams) => <>{params.value}</> },
 		{ field: 'category', headerName: 'Category', flex: 1, minWidth: 150, renderCell: (params: GridRenderCellParams) => <>{params.value}</> },
 		{ field: 'company', headerName: 'Company', flex: 1, minWidth: 150, renderCell: (params: GridRenderCellParams) => <>{params.value}</> },
